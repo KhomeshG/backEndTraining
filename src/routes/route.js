1623accc0fef6c491b28d9116381088bf09ef1b2
KhomeshG <khomesshgajbhiye@gmail.com>
2022-08-12 23:47:11 +0530

    Adding 12Aug Assignment in Repo

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,164 +1,181 @@
-const { json } = require('body-parser');
-const express = require('express');
+const { json } = require("body-parser");
+const express = require("express");
 const router = express.Router();
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
-})
+router.get("/students/:name", function (req, res) {
+  let studentName = req.params.name;
+  console.log(studentName);
+  res.send(studentName);
+});
 
-router.get("/random" , function(req, res) {
-    res.send("hi there")
-})
+router.get("/random", function (req, res) {
+  res.send("hi there");
+});
 
+router.get("/test-api", function (req, res) {
+  res.send("hi FunctionUp");
+});
 
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
+router.get("/test-api-2", function (req, res) {
+  res.send("hi FunctionUp. This is another cool API");
+});
 
+router.get("/test-api-3", function (req, res) {
+  res.send(
+    "hi FunctionUp. This is another cool API. And NOw i am bored of creating API's "
+  );
+});
 
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
+router.get("/test-api-4", function (req, res) {
+  res.send(
+    "hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s "
+  );
+});
 
+router.get("/test-api-5", function (req, res) {
+  res.send(
+    "hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s "
+  );
+});
 
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
+router.get("/test-api-6", function (req, res) {
+  res.send({ a: 56, b: 45 });
+});
 
+router.post("/test-post", function (req, res) {
+  res.send([23, 45, 6]);
+});
 
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
+router.post("/test-post-2", function (req, res) {
+  res.send({ msg: "hi", status: true });
+});
 
+router.post("/test-post-3", function (req, res) {
+  let id = req.body.user;
+  let pwd = req.body.password;
 
+  console.log(id, pwd);
 
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
+  console.log(req.body);
 
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
+  res.send({ msg: "hi", status: true });
+});
 
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
-})
-
-
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-router.post("/test-post-3", function(req, res) {
-    let id = req.body.user
-    let pwd= req.body.password
-
-    console.log( id , pwd)
-
-    console.log( req.body )
-
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-
-
-router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
-    arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
-})
+router.post("/test-post-4", function (req, res) {
+  let arr = [12, "functionup"];
+  let ele = req.body.element;
+  arr.push(ele);
+  res.send({ msg: arr, status: true });
+});
 //
-
 
 //Assignment 10Aug2022--------------->
 //----------------------------------------------
-let players=[{"name":"manish",
-            "dob":"1/1/1995",
-            "gender":"male",
-        "city":"jalandhar",
-        "sports":["swwimming"]},
-        {"name":"gopal",
-        "dob":"1/09/1995",
-        "gender":"male",
-    "city":"delhi",
-    "sports":["soccer"]},
-    {"name":"lokesh",
-    "dob":"1/1/1990",
-    "gender":"male",
-"city":"mumbai",
-"sports":["soccerr"]}]
+let players = [
+  {
+    name: "manish",
+    dob: "1/1/1995",
+    gender: "male",
+    city: "jalandhar",
+    sports: ["swwimming"],
+  },
+  {
+    name: "gopal",
+    dob: "1/09/1995",
+    gender: "male",
+    city: "delhi",
+    sports: ["soccer"],
+  },
+  {
+    name: "lokesh",
+    dob: "1/1/1990",
+    gender: "male",
+    city: "mumbai",
+    sports: ["soccerr"],
+  },
+];
 //Creating one flag globaly
-let nameIsExist=false
+let nameIsExist = false;
 //API For Adding Players Data
 
 //------
-router.post('/players', function (req, res) {
-    let queryParams = req.body 
-    //players data store inside of this variable which comming from body input
-    
-    for (i = 0; i < players.length; i++){
-        if (players[i].name === queryParams.name) {
-            //Accesing array of objects Dot method
-            nameIsExist=true 
-            break
-        }  
-    }
-    if (nameIsExist) {
-        //true
-        res.send("Name is Exist")
-    }
-    else {
-        //false
-        players.push(queryParams)
-        res.send(players)
-        //res.send() function does not allow numeric numbers or undefine
-    }
-    //to see application is working fine or not 
-    console.log("----------------new Output--------------------")
-    console.log(players)
+router.post("/players", function (req, res) {
+  let queryParams = req.body;
+  //players data store inside of this variable which comming from body input
 
-    
-
-})
+  for (i = 0; i < players.length; i++) {
+    if (players[i].name === queryParams.name) {
+      //Accesing array of objects Dot method
+      nameIsExist = true;
+      break;
+    }
+  }
+  if (nameIsExist) {
+    //true
+    res.send("Name is Exist");
+  } else {
+    //false
+    players.push(queryParams);
+    res.send(players);
+    //res.send() function does not allow numeric numbers or undefine
+  }
+  //to see application is working fine or not
+  console.log("----------------new Output--------------------");
+  console.log(players);
+});
 //------------------end----------------------------------
 
 //-----ApI for booking Slot for a Player
 //=================================================
-router.post('/players/:playerName', function (req, res) {
-    //in Above plyerName and bookingID is Path parems
-    let pathParames = req.params.playerName
-    //this will give me undefine bcz It is in Objesct format I Need to Convert this in Json formate 
-    let Jconvert = JSON.stringify(pathParames)
-    //req.params.playerName data is store inside of pathParames
-    for (i = 0; i < players.length; i++){
-        if (Jconvert.name===players.name) {
-            
-        }
-        
+router.post("/players/:playerName", function (req, res) {
+  //in Above plyerName and bookingID is Path parems
+  let pathParames = req.params.playerName;
+  //this will give me undefine bcz It is in Objesct format I Need to Convert this in Json formate
+  let Jconvert = JSON.stringify(pathParames);
+  //req.params.playerName data is store inside of pathParames
+  for (i = 0; i < players.length; i++) {
+    if (Jconvert.name === players.name) {
     }
-    //checking name is exist or not in our Array using Higher Order Function 
-    console.log(nameCheck)
-})
+  }
+  //checking name is exist or not in our Array using Higher Order Function
+  console.log(nameCheck);
+});
 
+//Aug_12
+//Voting Assignment
+//Post Api Asssignment 12AUG
+//-----------------------------------------------------------
 
+let person = [
+  { name: "KG", age: 12, votingStatus: false },
+  { name: "DA", age: 78, votingStatus: false },
+  { name: "MG", age: 45, votingStatus: false },
+  { name: "MG", age: 11, votingStatus: false },
+  { name: "Nk", age: 4, votingStatus: false },
+];
+//Given Array of object
+//Global
 
+router.post("/get", function (req, res) {
+  //Create query param and req handler
+  let queryParam = req.query.votingAge;
+  //User request assigned qureyParam
+  const result = person.filter((pers) =>
+    pers.age > queryParam
+      ? (pers.votingStatus = true)
+      : (pers.votingStatus = false)
+  );
+  //Filtering persons from their age using filter (i.e higher Order Function)
+  //changing their votingstatus
+  console.log(result);
+  res.send({ data: result, status: true });
+});
 
-
-
-
-
-
+//----------------------------------------
 // let arr = [25, 45, 54, 67, 20183, 32]
 // router.post('/filter', function (req, res) {
 //     let input1 = req.query
-    
+
 // })
-
-//Post Api Asssignment 12AUG
-
-
 
 module.exports = router;
