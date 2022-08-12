40c99624a1ccc43b0530c2dc38f6b9a3799a620f
KhomeshG <khomesshgajbhiye@gmail.com>
2022-08-12 19:53:42 +0530

    cimmiting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-const express = require('express');
+const express = require ('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', route);
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+//Connecting MangoDB
+mongoose.connect("mongodb+srv://KhomeshUser-456:fttFkNqqiYu79Rgv@cluster0.aybauye.mongodb.net/test",{
+    useNewUrlParser:true
+})
+.then(() => console.log("MongoDb Is Connected"))
+.catch(err => console.log(err))
+
+
+app.listen(process.env.PORT || 3001, function() {
+    console.log('Express app running on port ' + (process.env.PORT || 3001))
 });
