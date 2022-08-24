83902504f812314d23783aff022e0435ecd02e9b
KhomeshG <khomesshgajbhiye@gmail.com>
2022-08-25 01:00:54 +0530

    Aug 25 Small Assignment Done

--- src/index.js
+++ src/index.js
@@ -1,29 +1,46 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const route = require('./routes/route.js');
-const { default: mongoose } = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const route = require("./routes/route.js");
+const { default: mongoose } = require("mongoose");
 const app = express();
+const moment = require("moment");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose
+  .connect(
+    "mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => console.log("MongoDb is connected"))
+  .catch((err) => console.log(err));
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-})
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+// app.use(function (req, res, next) {
+//   console.log("inside GLOBAL MW");
+//   next();
+// });
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
+//
+app.use(function (req, res, next) {
+  console.log("This is global MiddleWare");
+  //
+  let date = moment().format("YYYY-MM-DD HH:mm:ss");
+  //moment is package of node.js
 
-app.use('/', route);
+  let ipAddress = req.ip;
+  //req.ip gives the ip address of local machine
+  let urlThatAreInUse = req.originalUrl;
+  //req.orignalUrl gives the url which you are hiiting
+  console.log(date, ipAddress, urlThatAreInUse);
+  next();
+  //Next is a callBack function that do the next Operation
+});
 
+app.use("/", route);
 
 app.listen(process.env.PORT || 3000, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+  console.log("Express app running on port " + (process.env.PORT || 3000));
 });
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,32 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 // const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
-const commonMW = require ("../middlewares/commonMiddlewares")
+const UserController = require("../controllers/userController");
+const BookController = require("../controllers/bookController");
+const commonMW = require("../middlewares/commonMiddlewares");
 
 router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+  res.send("My first ever api!");
+});
 
+//<!-- ASSIGNMENT:- -->
+// Write a middleware that logs (console.log) some data everytime any API is hit
+// Data to be logged:-the current timestamp(as date time) , the IP of the user and the route being requested).
+// For this first figure out how to get the route location being requested, how to get current timestamp and how to get the IP.
+// NOTE: ip of local computer will come as ::1 so dont get disturbed by seeing this)
+router.get("/check", commonMW.middleWare, function (req, res) {
+  res.send("All Workng Fine");
+});
 
-
-
-router.post("/createBook", BookController.createBook  )
-
-
-
+router.post("/createBook", BookController.createBook);
 
 // router.post("/createUser", UserController.createUser  )
 // router.get("/getUsersData", UserController.getUsersData)
-
 
 // const mid1= function ( req, res, next) {
 //     console.log("Hi I am a middleware named Mid1")
 //     // logic
 //     let loggedIn = false
 
-//     if (loggedIn== true) { 
+//     if (loggedIn== true) {
 //         console.log( "OK LOGGED IS IS TRUE NOW")
 //         next ()
 //     }
@@ -44,19 +46,17 @@ router.post("/createBook", BookController.createBook  )
 // router.get('/termsAndConditions',  UserController.termsAndConditions)
 // router.get('/register',  UserController.register)
 
-
-
-
-
-router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
-
-
+router.get(
+  "/basicRoute",
+  commonMW.mid1,
+  commonMW.mid2,
+  commonMW.mid3,
+  commonMW.mid4,
+  UserController.basicCode
+);
 
 // router.get("/basicRoute2", commonMW.mid1, UserController.basicCode2)
 // router.get("/basicRoute3", commonMW.mid2, UserController.basicCode3)
 // router.get("/basicRoute4", commonMW.mid1, commonMW.mid4, UserController.basicCode4)
-
-
-
 
 module.exports = router;
